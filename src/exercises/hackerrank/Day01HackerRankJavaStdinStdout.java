eaf4e6af4843b58365dd1492dc3193b2cf6ac14c
gabrielTavaresFP <gabrieltavares8182pessoal@gmail.com>
2026-05-18 19:56:01 -0300

    feat : added HackerRank Java exercises

---
+++ src/exercises/hackerrank/Day01HackerRankJavaStdinStdout.java
@@ -0,0 +1,17 @@
+package exercises.hackerrank;
+
+import java.util.Scanner;
+
+public class Day01HackerRankJavaStdinStdout {
+    static void main() {
+        Scanner scan = new Scanner(System.in);
+        int a = scan.nextInt();
+        int b = scan.nextInt();
+        int c = scan.nextInt();
+        scan.close();
+
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+    }
+}
