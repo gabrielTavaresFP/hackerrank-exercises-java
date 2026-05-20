3449ffdfa1f2d7864b0b74a34fd0333e54e1b136
gabrielTavaresFP <gabrieltavares8182pessoal@gmail.com>
2026-05-19 22:11:02 -0300

    feat: added hacker rank loops, stdin and stdout exercises

---
+++ src/exercises/hackerrank/Day02HackerRankJavaLoops2.java
@@ -0,0 +1,33 @@
+package exercises.hackerrank;
+
+import java.util.*;
+import java.io.*;
+
+public class Day02HackerRankJavaLoops2 {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+
+        int t = in.nextInt();
+
+
+
+
+
+        for (int i = 0; i < t; i++) {
+            int a = in.nextInt();
+            int b = in.nextInt();
+            int n = in.nextInt();
+
+            int two = 2;
+            int zero = 0;
+            int tz = (int) Math.pow(two, zero);
+
+            int nm = n -= 1;
+            int tn = (int) Math.pow(two, nm);
+
+          //  System.out.println(a += tz * b += tn * b);
+        }
+        in.close();
+    }
+
+}
--- src/exercises/hackerrank/Day02HackerRankJavaStdinStdout2.java
+++ src/exercises/hackerrank/Day02HackerRankJavaStdinStdout2.java
@@ -1,7 +1,9 @@
 package exercises.hackerrank;
 
 public class Day02HackerRankJavaStdinStdout2 {
-    static void main() {
-
+    public  static void main(String[] args) {
+        int  i = 42;
+        double d = 3.1415;
+        String s = "Welcome to HackerRank's Java tutorials!";
     }
 }
