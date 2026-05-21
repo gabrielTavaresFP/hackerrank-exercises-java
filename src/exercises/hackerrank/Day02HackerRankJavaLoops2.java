25ec8cc9c368d44f6766dce8e363713eaec49dbd
gabrielTavaresFP <gabrieltavares8182pessoal@gmail.com>
2026-05-20 22:09:44 -0300

    feat: added more repetition exercises

--- src/exercises/hackerrank/Day02HackerRankJavaLoops2.java
+++ src/exercises/hackerrank/Day02HackerRankJavaLoops2.java
@@ -1,33 +1,16 @@
 package exercises.hackerrank;
 
-import java.util.*;
-import java.io.*;
+
 
 public class Day02HackerRankJavaLoops2 {
     public static void main(String[] args) {
-        Scanner in = new Scanner(System.in);
-
-        int t = in.nextInt();
-
-
-
-
-
-        for (int i = 0; i < t; i++) {
-            int a = in.nextInt();
-            int b = in.nextInt();
-            int n = in.nextInt();
-
-            int two = 2;
-            int zero = 0;
-            int tz = (int) Math.pow(two, zero);
-
-            int nm = n -= 1;
-            int tn = (int) Math.pow(two, nm);
-
-          //  System.out.println(a += tz * b += tn * b);
+        for (int n = 1; n < 9999999; n++) {
+           double firstEquation = 0 + 1 * 2;
+            int n1 = n - 1;
+            double secondEquation = Math.pow(2,n1) * 2;
+            System.out.println(firstEquation + secondEquation);
         }
-        in.close();
+
     }
 
 }
