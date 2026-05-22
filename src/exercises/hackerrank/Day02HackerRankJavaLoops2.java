e1bfc26362924577f47dfffb42c675678fe16abf
gabrielTavaresFP <gabrieltavares8182pessoal@gmail.com>
2026-05-21 21:52:52 -0300

    daily commit

--- src/exercises/hackerrank/Day02HackerRankJavaLoops2.java
+++ src/exercises/hackerrank/Day02HackerRankJavaLoops2.java
@@ -4,11 +4,7 @@ package exercises.hackerrank;
 
 public class Day02HackerRankJavaLoops2 {
     public static void main(String[] args) {
-        for (int n = 1; n < 9999999; n++) {
-           double firstEquation = 0 + 1 * 2;
-            int n1 = n - 1;
-            double secondEquation = Math.pow(2,n1) * 2;
-            System.out.println(firstEquation + secondEquation);
+
         }
 
     }
