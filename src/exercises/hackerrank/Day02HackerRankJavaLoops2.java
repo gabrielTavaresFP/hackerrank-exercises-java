4d78772bf0a481b6e0387f86535c50b19c13999d
gabrielTavaresFP <gabrieltavares8182pessoal@gmail.com>
2026-05-22 22:32:39 -0300

    daily commit

--- src/exercises/hackerrank/Day02HackerRankJavaLoops2.java
+++ src/exercises/hackerrank/Day02HackerRankJavaLoops2.java
@@ -1,12 +1,9 @@
 package exercises.hackerrank;
 
 
-
 public class Day02HackerRankJavaLoops2 {
     public static void main(String[] args) {
-
+        System.out.println("idk wt to do lol");
         }
-
-    }
 
 }
